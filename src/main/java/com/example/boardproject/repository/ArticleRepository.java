364e02eb459c77ca52ae67d1cf449c0a61b4e5cd
yang <gluestuck@naver.com>
2022-10-16 13:07:43 +0900

    #10 - repository interface 작성
    이걸로 db에 접근

---
+++ src/main/java/com/example/boardproject/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.example.boardproject.repository;
+
+import com.example.boardproject.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+}
